7135b3733d9d941f10cec78168c6f186add908af
Marco Barbosa <marcobarbosa@outlook.com>
2021-03-16 13:53:28 +0100

    setup initial structure

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask("Hello World")
+
+
+@app.route('/home')
+def hello_world():
+	return render_template('index.html', name="Barbosa")
+
+
+@app.route("/test")
+def test():
+    return "success"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
