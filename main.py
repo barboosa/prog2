5a63dbc9792d86b72eea1a95ff64a35eba3d9032
Marco Barbosa <marcobarbosa@outlook.com>
2021-05-11 20:35:11 +0200

    added form for create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask
+from flask import Flask, request
 from flask import render_template
 
 app = Flask("Hello World")
@@ -21,21 +21,29 @@ def load_movie_data_by_id(movie_id):
     return movie
 
 @app.route('/')
-def load_index():
+def index():
     return render_template('index.html')
 
 @app.route('/movies')
-def load_movies():
+def movies():
     movies = load_movie_data()
     return render_template('movies.html', movies=movies)
 
 @app.route('/movie/<movie_id>')
-def load_movie_by_id(movie_id):
+def movie_by_id(movie_id):
     movie = load_movie_data_by_id(movie_id)
     return render_template('movie_details.html', movie=movie)
 
+@app.route('/movie/create', methods=['GET', 'POST'])
+def movie_create():
+    movies = load_movie_data()
+    if request.method == 'POST':
+        movie_create_data = request.form
+        return render_template('movies.html', movies=movies)
+    return render_template('movie_create.html')
+
 @app.route('/search')
-def load_search():
+def search():
     return render_template('search.html')
 
 if __name__ == "__main__":
